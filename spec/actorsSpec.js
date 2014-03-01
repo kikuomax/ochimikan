46202a1cbe991d37deb6c22610517831be86928e
Kikuo Emoto <kikuomax@gmail.com>
2014-03-01 18:39:31 +0900

    refined specification
    - refined messages of specification
    - refined specification of Actor.isActor

--- spec/actorsSpec.js
+++ spec/actorsSpec.js
@@ -1,5 +1,5 @@
 describe('Actor', function() {
-    it('Should have priority', function() {
+    it('Should have a priority', function() {
 	var actor = new Actor(0, function(s){});
 	expect(actor.priority).toBe(0);
 	actor = new Actor(1, function(s){});
@@ -8,13 +8,13 @@ describe('Actor', function() {
 	expect(actor.priority).toBe(-1);
     });
 
-    it('Should have specified action', function() {
+    it('Should have a specified action', function() {
 	var act = function(s) {};
 	var actor = new Actor(1, act);
 	expect(actor.act).toBe(act);
     });
 
-    it('Should throw exception if act is not a function', function() {
+    it('Should throw an exception if act is not a function', function() {
 	expect(function() {
 	    new Actor(0, {});
 	}).toThrow("act must be a function");
@@ -25,8 +25,13 @@ describe('Actor', function() {
 	expect(Actor.isActor(actor)).toBe(true);
     });
 
-    it('isActor should be false for an empty object', function() {
-	var actor = {};
+    it('isActor should be false for an object which lacks priority', function() {
+	var actor = { act: function(s){} };
+	expect(Actor.isActor(actor)).toBe(false);
+    });
+
+    it('isActor should be false for an object which lacks act', function() {
+	var actor = { priority: 0 };
 	expect(Actor.isActor(actor)).toBe(false);
     });
 
@@ -38,7 +43,7 @@ describe('Actor', function() {
 	expect(Actor.isActor(undefined)).toBe(false);
     });
 
-    it('makeActor should make object actor', function() {
+    it('makeActor should make an object an actor', function() {
 	var obj = {};
 	var act = function(s) {};
 	expect(Actor.makeActor(obj, 0, act)).toBe(obj);
@@ -71,14 +76,14 @@ describe('Actor', function() {
 				       new Actor(1, nop))).toBe(0);
     });
 
-    it('comparePriorities should return negative number if lhs.priority < rhs.priority', function() {
+    it('comparePriorities should return a negative number if lhs.priority < rhs.priority', function() {
 	expect(Actor.comparePriorities(new Actor(0, nop),
 				       new Actor(1, nop))).toBeLessThan(0);
 	expect(Actor.comparePriorities(new Actor(-1, nop),
 				       new Actor(0, nop))).toBeLessThan(0);
     });
 
-    it('comparePriorities should return positive number if lhs.priority > rhs.priority', function() {
+    it('comparePriorities should return a positive number if lhs.priority > rhs.priority', function() {
 	expect(Actor.comparePriorities(new Actor(0, nop),
 				       new Actor(-1, nop))).toBeGreaterThan(0);
 	expect(Actor.comparePriorities(new Actor(1, nop),
@@ -90,12 +95,12 @@ describe('Actor', function() {
 });
 
 describe('ActorScheduler', function() {
-    it('Should have empty actor queue', function() {
+    it('Should have an empty actor queue', function() {
 	var scheduler = new ActorScheduler();
 	expect(scheduler.actorQueue.length).toBe(0);
     });
 
-    it('Can schedule actor', function() {
+    it('Can schedule an actor', function() {
 	var scheduler = new ActorScheduler();
 	var actor = new Actor(0, function(s) {});
 	scheduler.schedule(actor);
@@ -103,7 +108,7 @@ describe('ActorScheduler', function() {
 	expect(scheduler.actorQueue).toContain(actor);
     });
 
-    it('isActorScheduler should be true for actor scheduler', function() {
+    it('isActorScheduler should be true for an actor scheduler', function() {
 	var scheduler = new ActorScheduler();
 	expect(ActorScheduler.isActorScheduler(scheduler)).toBe(true);
     });
@@ -140,7 +145,7 @@ describe('ActorScheduler', function() {
 	expect(ActorScheduler.isActorScheduler(obj)).toBe(true);
     });
 
-    it('makeActorScheduler should replace properties of object', function() {
+    it('makeActorScheduler should replace properties of the target', function() {
 	var obj = {
 	    actorQueue: [{}]
 	};
@@ -167,14 +172,14 @@ describe('run scheduled actors', function() {
 	scheduler = undefined;
     });
 
-    it('actor scheduler should run scheduled actor and delete it from queue. priority = 0', function() {
+    it('actor scheduler should run a scheduled actor and delete it from the queue. priority = 0', function() {
 	scheduler.schedule(new Actor(0, act1));
 	scheduler.run();
 	expect(act1).toHaveBeenCalledWith(scheduler);
 	expect(scheduler.actorQueue.length).toBe(0);
     });
 
-    it('actor scheduler should run scheduled actors and delete them from queue. priorities = 0, 0, 0', function() {
+    it('actor scheduler should run scheduled actors and delete them from the queue. priorities = 0, 0, 0', function() {
 	scheduler.schedule(new Actor(0, act1));
 	scheduler.schedule(new Actor(0, act2));
 	scheduler.schedule(new Actor(0, act3));
@@ -185,7 +190,7 @@ describe('run scheduled actors', function() {
 	expect(scheduler.actorQueue.length).toBe(0);
     });
 
-    it('actor scheduler should run scheduled actors and delete them from queue. priorities = 1, 2, 1', function() {
+    it('actor scheduler should run scheduled actors and delete them from the queue. priorities = 1, 2, 1', function() {
 	var leftActor;
 	scheduler.schedule(new Actor(1, act1));
 	scheduler.schedule(leftActor = new Actor(2, act2));
@@ -196,7 +201,7 @@ describe('run scheduled actors', function() {
 	expect(scheduler.actorQueue).toEqual([leftActor]);
     });
 
-    it('actor scheduler should run scheduled actors and delete them from queue. priorities = -1, 0, 1', function() {
+    it('actor scheduler should run scheduled actors and delete them from the queue. priorities = -1, 0, 1', function() {
 	var leftActor;
 	scheduler.schedule(new Actor(-1, act1));
 	scheduler.schedule(new Actor(0, act2));
